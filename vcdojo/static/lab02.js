817b7601eedf000fc6f72bf57b5a8550f8e6edb8
Noe Javet <np.javet@gmail.com>
2025-01-08 18:02:36 +0100

    clean up

--- vcdojo/static/lab02.js
+++ vcdojo/static/lab02.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+//import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,
