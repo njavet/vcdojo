4ad05c0ba281da2a5f792e0dab1c98ecaedf571b
Noe Javet <np.javet@gmail.com>
2025-01-08 21:21:26 +0100

    wip

--- src/views/Lab2.vue
+++ src/views/Lab2.vue
@@ -15,22 +15,56 @@
       </div>
     </div>
     <div id="controls">
-      <button class="color-btn" data-color="#6312ff">Purple</button>
-		  <button class="color-btn" data-color="#ffffff">White</button>
-		  <button class="color-btn" data-color="#000000">Black</button>
-		  <button class="color-btn" data-color="#c0c0c0">Silver</button>
-		  <button id="rotateHorizontal" class="switch-btn">Toggle Horizontal Rotation</button>
-		  <button id="rotateVertical" class="switch-btn">Toggle Vertical Rotation</button>
-		  <button id="prev-model" class="switch-btn">Previous Model</button>
-      <button id="next-model" class="switch-btn">Next Model</button>
+      <button @click="switchModel('prev')">Previous Model</button>
+      <button @click="switchModel('next')">Next Model</button>
+      <button @click="toggleRotation('horizontal')">Toggle Horizontal Rotation</button>
+      <button @click="toggleRotation('vertical')">Toggle Vertical Rotation</button>
+    </div>
+    <div id="color-controls">
+      <button v-for="color in colors" :key="color" :data-color="color" @click="changeModelColor(color)">
+        {{ color }}
+      </button>
     </div>
       <router-link to="/">Go to Home</router-link>
   </div>
 </template>
 
 <script>
+import { initializeScene, loadModel, changeModelColor } from '../js/lab2.js';
 export default {
   name: 'Lab2',
+  data() {
+    return {
+      currentModelIndex: 0,
+      isRotatingHorizontal: false,
+      isRotatingVertical: false,
+      colors: ['#ff0000', '#00ff00', '#0000ff', '#ffff00'],
+    }
+  },
+  mounted() {
+    // Initialize the Three.js scene
+    const container = this.$el;
+    initializeScene(container);
+  },
+  methods: {
+    switchModel(direction) {
+      if (direction === 'next') {
+        this.currentModelIndex = (this.currentModelIndex + 1) % models.length;
+      } else if (direction === 'prev') {
+        this.currentModelIndex = (this.currentModelIndex - 1 + models.length) % models.length;
+      }
+      loadModel(this.currentModelIndex);
+    },
+    toggleRotation(axis) {
+      if (axis === 'horizontal') {
+        this.isRotatingHorizontal = !this.isRotatingHorizontal;
+      } else if (axis === 'vertical') {
+        this.isRotatingVertical = !this.isRotatingVertical;
+      }
+    },
+    changeModelColor(color) {
+      changeModelColor(color);
+    },
+  }
 };
-
 </script>
