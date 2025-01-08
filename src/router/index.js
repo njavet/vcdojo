33821ceb49a8aad7b0a8bfbc9f0cc6310239b603
Noe Javet <np.javet@gmail.com>
2025-01-08 19:23:46 +0100

    styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
+import Lab0 from '../views/Lab0.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
+  { path: '/lab0', component: Lab0 },
 ];
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
     <h1>Visual Computing Dojo</h1>
     <p>Welcome to the Visual Computing Dojo!</p>
     <router-link to="/about">Go to About</router-link>
+    <br>
+    <router-link to="/lab0">Go to Lab </router-link>
   </div>
 </template>
 
