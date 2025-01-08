3040fc5b001e29496ad4fc6d6bea2adae8fc5e73
Noe Javet <np.javet@gmail.com>
2025-01-08 19:43:45 +0100

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,11 @@
     <p>Welcome to the Visual Computing Dojo!</p>
     <router-link to="/about">Go to About</router-link>
     <br>
-    <router-link to="/lab0">Go to Lab </router-link>
+    <router-link to="/lab0">Go to Lab 0 </router-link>
+    <br>
+    <router-link to="/lab1">Go to Lab 1 </router-link>
+    <br>
+    <router-link to="/lab2">Go to Lab 2 </router-link>
   </div>
 </template>
 
