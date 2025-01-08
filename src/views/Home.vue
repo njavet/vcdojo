6d5bfd6eb38a7bd4af372128d27987d41efd360d
Noe Javet <np.javet@gmail.com>
2025-01-08 19:10:35 +0100

    add views

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to the Home Page!</p>
+    <router-link to="/about">Go to About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
