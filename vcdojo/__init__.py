9580ef83184886fdcaa7e0f4de4825bbf4eb9be7
Noe Javet <np.javet@gmail.com>
2025-01-08 11:52:58 +0100

    base web framework

---
+++ vcdojo/__init__.py
@@ -0,0 +1,12 @@
+import uvicorn
+
+# project imports
+from .main import create_app
+
+
+def run_app():
+    uvicorn.run('vcdojo:create_app',
+                port=8080,
+                reload=True,
+                factory=True,
+                log_level='debug')
---
+++ vcdojo/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from pathlib import Path
+
+
+def create_app():
+    app = FastAPI()
+    app.mount("/static", StaticFiles(directory='vcdojo/static'), name='static')
+
+    @app.get("/", response_class=HTMLResponse)
+    def read_root():
+        html_path = Path('vcdojo/templates/index.html').read_text()
+        return HTMLResponse(content=html_path)
+
+    return app
