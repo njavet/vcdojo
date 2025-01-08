7ed827d58fca614b9e7dcb4d1b88d5c90e8de00d
Noe Javet <np.javet@gmail.com>
2025-01-08 17:57:42 +0100

    fixing bundles

--- vcdojo/static/lab02.js
+++ vcdojo/static/lab02.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://127.0.0.1:8080', // Proxy API calls to FastAPI
-    },
-  },
-});
