3526e42ff849bea897599f74d27a01d916442610
Noe Javet <np.javet@gmail.com>
2025-01-09 17:38:54 +0100

    css play

--- src/views/Lab2.vue
+++ src/views/Lab2.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <h1>Tycho Station</h1>
+    <div id="title">Tycho Station Weapon Shop</div>
     <div id="stats-container">
       <div class="stats-box">
         <span><strong>Price:</strong>64</span>
@@ -27,13 +27,13 @@
 </template>
 
 <script>
-//import '../styles/lab2.css';
+import '../styles/lab2.css';
 import { initializeScene, toggle_rotation, switch_model, change_model_color} from '../js/lab2.js';
 export default {
   name: 'Lab2',
   data() {
     return {
-      colors: ['#ffffff', '#000000', '#6312ff', '#950000'],
+      colors: ['#ffffff', '#000000', 'grey', '#950000'],
     }
   },
   mounted() {
